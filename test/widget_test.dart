5e1215faab42127a3b10ceda606b8eed0c8890e6
tshsu <sandy31272325@gmail.com>
2025-08-21 02:42:31 +0800

    Skip flutter test template

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:photo_coach/app.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  testWidgets('skip template counter test', (tester) async {
+    // 你可以在未來寫自己的測試，暫時先跳過模板
+  }, skip: true);
 }
